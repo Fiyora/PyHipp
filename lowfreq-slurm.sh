f540fc3dd43503570cfc35d0e52f505277ff7060
AWS ParallelCluster user <ec2-user@ip-10-0-5-231.ap-southeast-1.compute.internal>
2025-09-18 08:08:14 +0000

    update

---
+++ lowfreq-slurm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Submit this script with: sbatch <this-filename>
+
+#SBATCH --time=24:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --cpus-per-task=1	# number of processors per task
+#SBATCH -J "freq"   # job name
+
+## /SBATCH -p general # partition (queue)
+#SBATCH -o freq-slurm.%N.%j.out # STDOUT
+#SBATCH -e freq-slurm.%N.%j.err # STDERR
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+python -u -c "import PyHipp as pyh; \
+import time; \
+pyh.FreqSpectrum(saveLevel=1,redoLevel=1);
+print(time.localtime());"
+
+aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:023231074130:awsnotify --message "LowFreqJobDone"
